da153ae12ef2448c699cc93fed8850fd5f4030f5
Susana <superuca.dark@gmail.com>
2021-04-26 17:20:22 +0100

    aula 03

--- README.md
+++ README.md
@@ -1,63 +1,62 @@
 <img src="https://github.com/sestevao/podcastrnext/blob/main/.github/logo.svg" align="center"/>
-
 <h1 align="center">Podcastrnext</h1>
 
-### About 
+<br>
 
-Podcastr is a platform built for podcast broadcasting. Podcastrnext to bring you even closer to the best technology podcasts!
-This is a project developed during the Next Level Week 5th, presented by @Rocketseat during April 19-25, 2021
+# 👀 About 
 
-### Funcionalidades
+Podcastr is a platform built for podcast broadcasting. Podcastrnext to bring you even closer to the best technology podcasts! This is a project developed during Application built on the ** React.js ** track from ** Next Level Week # 05 ** by [@rocketseat](https://rocketseat.com.br/) during days 19 to 25 April 2021.
 
-- Listar todos os episódios
-- Listar os dois últimos lançamentos
-- Criar um player para executar o podcast
-- Criar uma página de conteúdo para cada podcast
+<br>
 
+# 🚀 Features
 
-### Documentos
+- List all episodes
+- List the last two releases
+- Create a player to play the podcast
+- Create a content page for each podcast
 
-[Figma](https://www.figma.com/file/UwFEntsHpHYJlHNQAQr4gA/Podcastr/duplicate?node-id=160%3A2761)
+<br>
 
-[Notion](https://www.notion.so/Trilha-ReactJS-9e6bfe82f2d047fa805935a3242e7952)
+# 💻 Technologies
 
-[Deploy Podcastr](https://www.notion.so/Deploy-Podcastr-2142f78ad75c4b32b2e4dc9e22c46189)
-
-[Os meus apontamentos](https://www.notion.so/Trilha-ReactJS-com-NextJS-fac35cbd50f04d47a2908699ba0dd91c)
-
-
-### 🧪 Tecnologias
-
-- [react]
-- [typescript]
-- [next.js]
+- [ReactJS](https://reactjs.org/)
+- [Typescript](https://www.typescriptlang.org/)
+- [Next.js](https://nextjs.org/)
 - [SASSS](https://sass-lang.com/)
-- [date-fns]
-- [json-server]
+- [date-fns](https://date-fns.org/)
+- [json-server](https://github.com/typicode/json-server)
+- [axios](https://github.com/axios/axios)
 
-### Pre requisitos
+<br>
 
-- [Git]
-- [node.js]
-- [yarn]
-- [vscode]
-- 
+# 🛠 Prerequisites
 
-### 🚀 Commands
+- [Git](https://git-scm.com/)
+- [Node.js](https://nodejs.org/en/)
+- [Yarn](https://yarnpkg.com/)
+- [vscode](https://code.visualstudio.com/)
+
+<br>
+
+# 🔖 Documents
+
+- [Figma](https://www.figma.com/file/UwFEntsHpHYJlHNQAQr4gA/Podcastr/duplicate?node-id=160%3A2761)
+- [Notion](https://www.notion.so/Trilha-ReactJS-9e6bfe82f2d047fa805935a3242e7952)
+- [Deploy Podcastr](https://www.notion.so/Deploy-Podcastr-2142f78ad75c4b32b2e4dc9e22c46189)
+- [My notes](https://www.notion.so/Trilha-ReactJS-com-NextJS-fac35cbd50f04d47a2908699ba0dd91c)
+
+<br>
+
+# 👷 Getting started
 
 ```bash
-$ npx create-react-app podcastr
-$ npx create-next-app podcastrnext
-```
-
-
-- Criar build no projeto
-```bash
-# Cria build no projeto
-$ yarn build
-# inicia o projeto como desenvolvimento
+# Clone the project 
+$ git clone https://github.com/sestevao/podcastrnext
+# Access the folder
+$ cd podcastrnext
+# Install the dependencies
+$ yarn 
+# Start the project
 $ yarn dev
-# inicia o projeto como producao
-$ yarn start
 ```
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,190 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { format, parseISO } from 'date-fns'
+
+import { convertDurationToTimeString } from '../utils/ConvertDurationToTimeString'
+import { api } from '../services/api'
+import styles from './home.module.scss'
+
+type Episode = {
+  id: string
+  title: string
+  publishedAt: string
+  thumbnail: string
+  members: string
+  durationAsString: string
+  duration: number
+  url: string
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[],
+  allEpisodes: Episode[]
+}
+
+//chamada API em SSG(Static site generator) => faz o mesmo que o SSR porém mudando o nome da função e 
+//passando no return a opção de revalidate com os segundos para acontecer o recarregamento da api
+//então com este metodo quando uma pessoa acessar a home, é gerado um html estático que será mostrado para
+//as proximas pessoa que acessarem o site, e mudará apenas quando a api carregar novamente, assim repetindo o processo
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
-    </>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>últimos lançamentos</h2>
+         
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="./play-green.svg" alt="Play episode" />
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>Todos os episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="./play-green.svg" alt="Play episode" />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+
+          </tbody>
+        </table>
+      </section>
+    </div>
   )
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: "published_ad",
+      _order: "desc"
+    }
+  })
+  //const data = response.data
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy'),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      duration: Number(episode.file.duration),
+      url: episode.file.url,
+    }
+  })
+
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.length)
 
   return {
     props: {
-      episodes: data
+      latestEpisodes,
+      allEpisodes
     },
-    revalidate:  60 * 60 * 8, //a cada 8 horas gera uma nova versao
+    revalidate: 60 * 60 * 8, //a cada 8 horas gera uma nova versao
   }
 }
+
+
+//SPA:
+/* export default function Home(props) {
+  chamada API no formato SPA (ñ é indexável à google pois "demora" um pouco para carregar)
+    useEffect(() => {
+      fetch('http://localhost:3333/episodes')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, [])
+  return (
+      <div>
+          <h1>Index</h1>
+          <p>{JSON.stringify(props.episodes)}</p>
+      </div>
+  )
+} */
+
+/* SSR:
+chamada API em SSR(Server side rendering) => basta ir em qualquer arquivo da pasta pages e fazer o seguinte:
+executa a função toda vez que a pagina é carregada por alguem
+export default function Home(props) {
+  return (
+      <div>
+          <h1>Index</h1>
+          <p>{JSON.stringify(props.episodes)}</p>
+      </div>
+  )
+}
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+} */
