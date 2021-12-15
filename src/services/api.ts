a5a03b5551cf381f31bb8f48c68ecf7a18c70547
Susana <9211533+sestevao@users.noreply.github.com>
2021-12-15 10:43:18 +0000

    update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,5 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const api = axios.create({
-<<<<<<< HEAD
-  baseURL: process.env.API_URL || 'http://localhost:3333/',
-})
-=======
-  baseURL: 'http://localhost:3333'
-})
->>>>>>> 507607f1e64efcf27fd7cb5a404d0eae1e02ced1
+  baseURL: process.env.API_URL || "http://localhost:3333/",
+});
